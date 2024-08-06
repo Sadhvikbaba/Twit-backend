912544fd2fe0b29a41a860b61df1805a9c74c93c
“Sadhvikbaba” <psadhvik2006@gmail.com>
2024-08-06 20:08:43 +0530

    user commit

--- src/middlewares/auth.middleware.js
+++ src/middlewares/auth.middleware.js
@@ -1,7 +1,7 @@
 import { asyncHandler } from "../utils/asyncHandler.js";
 import {ApiError} from "../utils/ApiError.js"
 import jwt from "jsonwebtoken";
-import { User } from "../models/user.models.js";
+import { User } from "../models/User.models.js";
 
 export const verifyJWT = asyncHandler(async (req , res , next) => {
         const token = req.cookies?.accessToken || req.header("Authorization")?.replace("Bearer " , "");
