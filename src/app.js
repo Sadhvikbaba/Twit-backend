d61fadf09d1c0ef9f2b2e3511c1254dc33c952a9
Sadhvik baba Patibandla <128380957+Sadhvikbaba@users.noreply.github.com>
2024-08-07 00:18:49 +0530

    Update app.js

--- src/app.js
+++ src/app.js
@@ -7,6 +7,8 @@ const app = express()
 app.use(cors({
     origin: process.env.CORS_ORIGIN,
     credentials: true,
+    allowedHeaders: ['Content-Type', 'Authorization', 'x-csrf-token'],
+    methods: ['GET', 'POST', 'PATCH', 'DELETE'],
 }));
 
 app.use(express.json({limit : "16kb"}))
