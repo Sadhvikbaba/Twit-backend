0e0d42da0c3870587e0b2784bc1bc89e61e6ab0e
Sadhvik baba Patibandla <128380957+Sadhvikbaba@users.noreply.github.com>
2024-08-07 21:47:23 +0530

    Update app.js

--- src/app.js
+++ src/app.js
@@ -1,6 +1,7 @@
 import express, { urlencoded } from "express";
 import cookieParser from "cookie-parser";
 import cors from "cors";
+import errorHandler from "./middlewares/error-handler.js";
 
 const app = express()
 
@@ -16,6 +17,7 @@ app.use(express.json({limit : "16kb"}))
 app.use(express.urlencoded({extended : true , limit : "16kb"}))
 app.use(express.static("public"))
 app.use(cookieParser())
+app.use(errorHandler)
 
 //routes import
 
