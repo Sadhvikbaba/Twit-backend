ca9646216115c9b5a38bbe8d250d1fa924c30d53
Sadhvik baba Patibandla <128380957+Sadhvikbaba@users.noreply.github.com>
2024-08-07 14:26:44 +0530

    Updated multer.middleware.js

--- src/middlewares/multer.middleware.js
+++ src/middlewares/multer.middleware.js
@@ -1,20 +1,12 @@
-import multer from 'multer';
-import path from 'path';
-import fs from 'fs';
-
-const uploadPath = path.join(__dirname, 'public/temp');
-
-if (!fs.existsSync(uploadPath)) {
-  fs.mkdirSync(uploadPath, { recursive: true });
-}
+import multer from "multer";
 
 const storage = multer.diskStorage({
-  destination: function (req, file, cb) {
-    cb(null, uploadPath);
-  },
-  filename: function (req, file, cb) {
-    cb(null, file.originalname);
-  }
-});
+    destination : function (req , res , cb) {
+        cb(null , './public/temp')
+    },
+    filename : function (req , file , cb){
+        cb(null , file.originalname)
+    }
+})
 
-export const upload = multer({ storage: storage });
+export const upload = multer({ storage : storage })
