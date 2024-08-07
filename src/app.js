8c4e8715cfaefddf2bc41b60f841c06adbf364ef
Sadhvik baba Patibandla <128380957+Sadhvikbaba@users.noreply.github.com>
2024-08-07 22:06:31 +0530

    Update app.js

--- src/app.js
+++ src/app.js
@@ -17,7 +17,7 @@ app.use(express.json({limit : "16kb"}))
 app.use(express.urlencoded({extended : true , limit : "16kb"}))
 app.use(express.static("public"))
 app.use(cookieParser())
-app.use(errorHandler)
+
 
 //routes import
 
@@ -42,5 +42,7 @@ app.use("/api/v1/comments", commentRouter)
 app.use("/api/v1/likes", likeRouter)
 app.use("/api/v1/playlist", playlistRouter)
 app.use("/api/v1/dashboard", dashboardRouter)
+
+app.use(errorHandler)
 
 export {app}
