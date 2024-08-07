44678405ea8616bf03356a965979d42c15d228e3
Sadhvik baba Patibandla <128380957+Sadhvikbaba@users.noreply.github.com>
2024-08-07 14:52:45 +0530

    Update final multer.middleware.js

--- src/middlewares/multer.middleware.js
+++ src/middlewares/multer.middleware.js
@@ -1,12 +1,19 @@
-import multer from "multer";
+import multer from 'multer';
+import { fileURLToPath } from 'url';  
+import { dirname } from 'path';       
+
+// Convert import.meta.url to a file path
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = dirname(__filename);
 
 const storage = multer.diskStorage({
-    destination : function (req , res , cb) {
-        cb(null , './public/temp')
+    destination: function (req, file, cb) {
+        // Use __dirname to construct the path
+        cb(null, `${__dirname}/public/temp`);
     },
-    filename : function (req , file , cb){
-        cb(null , file.originalname)
+    filename: function (req, file, cb) {
+        cb(null, file.originalname);
     }
-})
+});
 
-export const upload = multer({ storage : storage })
+export const upload = multer({ storage });
