0d6992fbb7bc1874a42c264cf09d7c42ebb3fdfd
Sadhvik baba Patibandla <128380957+Sadhvikbaba@users.noreply.github.com>
2024-08-07 15:29:38 +0530

    hope it works

--- src/middlewares/multer.middleware.js
+++ src/middlewares/multer.middleware.js
@@ -1,16 +1,8 @@
 import multer from 'multer';
-import { fileURLToPath } from 'url';
-import { dirname, resolve } from 'path';
-
-const __filename = fileURLToPath(import.meta.url);
-const __dirname = dirname(__filename);
-
-
-const tempDirPath = resolve(__dirname, '../../public/temp'); 
 
 const storage = multer.diskStorage({
     destination: function (req, file, cb) {
-        cb(null, tempDirPath); 
+        cb(null, '/tmp'); 
     },
     filename: function (req, file, cb) {
         cb(null, file.originalname);
