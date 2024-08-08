f9c8bfb9c29edce8bb7d5403ff923df7877529c7
Sadhvik baba Patibandla <128380957+Sadhvikbaba@users.noreply.github.com>
2024-08-08 21:47:31 +0530

    Update constants.js

--- src/constants.js
+++ src/constants.js
@@ -1,6 +1,2 @@
 export const DB_NAME = "sadhvik_1"
 
-//mongodb+srv://psadhvik2006:<password>@cluster0.x2sxis2.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0
-
-
-//video : 6686abbee36ba675e95cb05f
